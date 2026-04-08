3689c72f5310470df9af3691e0b3b55439d01653
preston192 <160216456+preston192@users.noreply.github.com>
2026-04-08 17:11:14 -0400

    Create 1548-CheckIfAll1sAreAtLeastLengthKPlacesAway

---
+++ 1548-CheckIfAll1sAreAtLeastLengthKPlacesAway/1548-CheckIfAll1sAreAtLeastLengthKPlacesAway.py
@@ -0,0 +1,13 @@
+# Last updated: 4/8/2026, 5:11:14 PM
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        count = k
+
+        for num in nums:
+            if num == 1:
+                if count < k:
+                    return False
+                count = 0
+            else:
+                count += 1
+        return True
