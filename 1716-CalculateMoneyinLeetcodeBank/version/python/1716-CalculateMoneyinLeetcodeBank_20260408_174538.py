a3e7369ede1007b6dc38fe4af6f51532f9fcba62
preston192 <160216456+preston192@users.noreply.github.com>
2026-04-08 17:45:38 -0400

    Create 1716-CalculateMoneyinLeetcodeBank

---
+++ 1716-CalculateMoneyinLeetcodeBank/version/python/1716-CalculateMoneyinLeetcodeBank_20260408_174538.py
@@ -0,0 +1,16 @@
+# Last updated: 4/8/2026, 5:45:38 PM
+1class Solution:
+2    def totalMoney(self, n: int) -> int:
+3        total = 0
+4        full_weeks = n // 7
+5        stub_week = n - (full_weeks*7)
+6
+7        for i in range(full_weeks):
+8            total += 28
+9            total += i*7
+10
+11        for j in range(stub_week):
+12            total += j + 1 + full_weeks
+13            
+14        return total
+15
