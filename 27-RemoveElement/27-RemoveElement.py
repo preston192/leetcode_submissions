194a56f88231be1b0b7d6178fdf8ced656d80aa2
preston192 <160216456+preston192@users.noreply.github.com>
2026-04-08 17:13:43 -0400

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,11 @@
+# Last updated: 4/8/2026, 5:13:43 PM
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l, r = 0, len(nums)
+        while l < r:
+            if nums[l] == val:
+                nums[l] = nums[r - 1]
+                r += -1
+            else: 
+                l += 1
+        return r
