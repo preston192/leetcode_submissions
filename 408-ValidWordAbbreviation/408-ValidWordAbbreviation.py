88245f57aebc68f92c2eecf7ccd2fc5e6ce6e192
preston192 <160216456+preston192@users.noreply.github.com>
2026-04-08 17:12:21 -0400

    Create 408-ValidWordAbbreviation

---
+++ 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
@@ -0,0 +1,20 @@
+# Last updated: 4/8/2026, 5:12:21 PM
+class Solution:
+    def validWordAbbreviation(self, word, abbr):
+        i = j = 0
+        m, n = len(word), len(abbr)
+        while i < m and j < n:
+            if word[i] == abbr[j]:
+                i += 1
+                j += 1
+            elif abbr[j] == "0":
+                return False
+            elif abbr[j].isdigit():
+                k = j
+                while k < n and abbr[k].isdigit():
+                    k += 1
+                i += int(abbr[j:k])
+                j = k
+            else:
+                return False
+        return i == m and j == n
